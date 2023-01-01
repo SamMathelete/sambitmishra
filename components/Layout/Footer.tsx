96181f59d54ca2149a3749ee2fd324c37874a591
sambitmishra <21ec01021@iitbbs.ac.in>
2023-01-01 18:46:31 +0530

    added dark mode support

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -5,11 +5,12 @@ import linkedin from "../../assets/LinkedIn.png";
 import github from "../../assets/Github.png";
 import twitter from "../../assets/Twitter.png";
 import Link from "next/link";
+import { motion } from "framer-motion";
 
 const Footer: FC = () => {
   return (
     <footer className={classes.mainDiv}>
-      <div className={classes.subDiv}>
+      <motion.div whileHover={{ scale: 1.1 }} className={classes.subDiv}>
         <Link href="https://www.linkedin.com/in/thesambitmishra/">
           <Image className={classes.image} src={linkedin} alt="LinkedIn" />
         </Link>
@@ -18,16 +19,16 @@ const Footer: FC = () => {
             thesambitmishra
           </Link>
         </div>
-      </div>
-      <div className={classes.subDiv}>
+      </motion.div>
+      <motion.div whileHover={{ scale: 1.1 }} className={classes.subDiv}>
         <Link href="https://github.com/SamMathelete">
           <Image className={classes.image} src={github} alt="Github" />
         </Link>
         <div className={classes.text}>
           <Link href="https://github.com/SamMathelete">SamMathelete</Link>
         </div>
-      </div>
-      <div className={classes.subDiv}>
+      </motion.div>
+      <motion.div whileHover={{ scale: 1.1 }} className={classes.subDiv}>
         <Link href="https://twitter.com/thesambitmishra">
           <Image className={classes.image} src={twitter} alt="Twitter" />
         </Link>
@@ -36,7 +37,7 @@ const Footer: FC = () => {
             thesambitmishra
           </Link>
         </div>
-      </div>
+      </motion.div>
     </footer>
   );
 };
