86eacf5d3155e7f3f18b67f829db88340c618660
sambitmishra <21ec01021@iitbbs.ac.in>
2023-01-01 12:33:54 +0530

    small changes to the home page

--- screens/HomePage.tsx
+++ screens/HomePage.tsx
@@ -1,8 +1,6 @@
 import { FC } from "react";
 import HomeDesc from "../components/HomePage/HomeDesc";
 import HomeLogo from "../components/HomePage/HomeLogo";
-import Footer from "../components/Layout/Footer";
-import Header from "../components/Layout/Header";
 
 const HomePage: FC = () => {
   return (
