8ef1b0b997275cd8ef6255939dd123e6b419f0a4
Sambit Mishra <95030527+SamMathelete@users.noreply.github.com>
2023-11-27 13:55:43 +0530

    fixes

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -8,7 +8,7 @@ const AboutPage = () => {
     I build beautiful and fully functional Websites and Apps using ReactJS, React Native, NodeJS and Firebase.
      I am also a Competitive Algorithmic Programmer and an Electronics and Communications Engineer. I love to solve Maths and Engineering Problems.
      <br /><br />
-      Let's connect and hope that we will make a difference together!
+      Let&apos;s connect and hope that we will make a difference together!
   </div>
   )
   return (
